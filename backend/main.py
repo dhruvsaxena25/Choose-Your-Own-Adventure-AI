baacf936548ea73080ea207038da24833250bc7e
Dhruv-saxena-25 <saxena25dhruv@gmail.com>
2025-09-17 10:26:22 +0530

    fastapi main calling done

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI(
+    title = "Choose Your Own Adventure API",
+    description = "Api to generate cool stories using AI",
+    version = "0.1.0",
+    docs_url = "/docs",
+    redoc_url = "/redoc",
+
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= ["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    
